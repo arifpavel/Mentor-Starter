9f1db010ed61604955b1247f65c819660c57778a
Arif Pavel <pavelpatiya@gmail.com>
2021-04-06 12:13:24 +0600

    elementor-stubs added to local php config as reference

--- .php
+++ .php
@@ -1,5 +1,6 @@
 {
   "intelephense.stubs": [
-    "/vendor/giacocorsiglia/wordpress-stubs/wordpress-stubs.php"
+    "/vendor/giacocorsiglia/wordpress-stubs/wordpress-stubs.php",
+    "/vendor/arifpavel/elementor-stubs/elementor-stubs.php",
   ]
 }
